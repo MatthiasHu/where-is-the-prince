689c3da9567e86bd5ca9171e400cd8e67fd2da2d
Matthias Hutzler <matthias.ralph.hutzler@student.uni-augsburg.de>
2016-09-16 18:48:14 +0200

    optimization: dijkstra graph search

--- where-is-the-prince.hs
+++ where-is-the-prince.hs
@@ -1,30 +1,49 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
 import Control.Monad
-import Data.List (nub)
+import qualified Data.Set as Set
+import qualified Data.Map.Strict as Map
 
 type Room = Int
 data Dir  = L | R deriving (Show,Eq)
 
+-- Take the set of rooms where the prince could currently be
+-- as a node of a graph.
+type Node = Set.Set Room
+
 allRooms :: Int -> [Room]
 allRooms n = [0..n-1]
 
-allPrincessStrategies :: Int -> Int -> [[Room]]
-allPrincessStrategies n k = replicateM k (allRooms n)
+neighbours :: Int -> Node -> [(Room, Node)]
+neighbours n node = do
+  princess <- allRooms n
+  let newNode = Set.fromList . concatMap (evade n princess) $ Set.toList node
+  return (princess, newNode)
 
-isWinningStrategy :: Int -> [Room] -> Bool
-isWinningStrategy n xs = null $
-  foldr (\x -> nub . concatMap (evade x)) (allRooms n) xs
+evade :: Int -> Room -> Room -> [Room]
+evade n princess prince =
+  filter (/= princess) . filter (\x -> 0<=x && x<n) $ [prince-1, prince+1]
+
+-- modified Dijkstra algotizhm to list the traces (type [t])
+-- of all shortest paths from a to b
+dijkstra :: forall n t. Ord n => (n -> [(t, n)]) -> n -> n -> [[t]]
+dijkstra neighbs a b = map reverse $ go Set.empty (Map.singleton a [[]])
   where
-    evade :: Room -> Room -> [Room]
-    evade princess prince =
-      filter (/= princess) . filter (\x -> 0<=x && x<n) $ [prince-1, prince+1]
+    go :: Set.Set n -> Map.Map n [[t]] -> [[t]]
+    go out reached | b `Map.member` reached  = reached Map.! b
+                   | otherwise  = go newOut newReached
+      where
+        newOut = Set.union out (Map.keysSet reached)
+        newReached = Map.fromListWith (++) $ do
+          (n, tss) <- Map.toList reached
+          (t, n') <- neighbs n
+          return (n', map (t:) tss)
 
 shortestWinningStrategies :: Int -> [[Room]]
-shortestWinningStrategies n = go 1
-    where
-    go k = if null ss then go (k+1) else ss
-        where ss = filter (isWinningStrategy n) (allPrincessStrategies n k)
+shortestWinningStrategies n =
+  dijkstra (neighbours n) (Set.fromList (allRooms n)) Set.empty
 
 bestWinningStrategy :: Int -> [Room]
 bestWinningStrategy = head . shortestWinningStrategies
